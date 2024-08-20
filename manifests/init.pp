586025d43fe4078cf5446728611d4d1a3386ed7f
Matthew Alexander LaChance <mattlach@umich.edu>
2024-08-20 10:26:06 -0400

    pdk new module fwknop

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+# Copyright (c) 2024 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include fwknop
+class fwknop {
+}
